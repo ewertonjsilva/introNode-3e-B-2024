01c589e987e12723cf4881c19288f571950ea554
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-09-09 09:03:39 -0300

    aula 02-09

--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,21 @@ const router = express.Router();
 
 // referência a controllers que serão utilizados nas rotas
 const UsuariosController = require('../controllers/usuarios'); 
+const ProdutosController = require('../controllers/produtos'); 
+const CidadesController = require('../controllers/cidades');
 
 // definição das rotas
 router.get('/usuarios', UsuariosController.listarUsuarios); 
 router.post('/usuarios', UsuariosController.cadastrarUsuarios); 
 router.patch('/usuarios', UsuariosController.editarUsuarios); 
 router.delete('/usuarios', UsuariosController.apagarUsuarios); 
+
+router.get('/produtos', ProdutosController.listarProdutos); 
+router.post('/produtos', ProdutosController.cadastrarProdutos); 
+router.patch('/produtos', ProdutosController.editarProdutos); 
+router.delete('/produtos', ProdutosController.apagarProdutos); 
+
+router.get('/cidades', CidadesController.listarCidades); 
 
 
 module.exports = router;
